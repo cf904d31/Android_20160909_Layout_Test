2e0a831e44375c4cdf0ec611c292ab390117d0fd
cf904d31 <cf904d31@gmail.com>
2016-09-09 13:42:53 +0800

    20160909_Layout_Test

---
+++ app/src/main/java/iii/org/tw/a20160909_layouttest/MainActivity.java
@@ -0,0 +1,13 @@
+package iii.org.tw.a20160909_layouttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
